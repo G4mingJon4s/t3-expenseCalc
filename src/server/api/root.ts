158cc0d0ec904f4f4af3e070b0781af086794573
G4mingJon4s <Jthelker@gmail.com>
2023-02-07 20:01:17 +0100

    added paymentRouter to trpc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
+import { paymentRouter } from "./routers/paymentRouter";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +8,8 @@ import { exampleRouter } from "./routers/example";
  * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+	example: exampleRouter,
+	payment: paymentRouter
 });
 
 // export type definition of API
