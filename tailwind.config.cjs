65c19dcf0a85b01d7fc9ce6152d198ac927cb46e
G4mingJon4s <Jthelker@gmail.com>
2023-02-07 19:58:17 +0100

    tailwind config stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,24 +2,27 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: "#ffffff",
+				gray: {
+					50: "#050505",
+					100: "#101010",
+					200: "#202020",
+					300: "#303030",
+					400: "#404040",
+					500: "#505050"
+				},
+				positive: "#90ee90",
+				negative: "#ff6666",
+				neutral: "#ffffff"
+			}
+		},
 		screens: {
 			sm: "480px",
 			md: "760px",
 			lg: "1040px",
 			xl: "1440px"
-		},
-		colors: {
-			white: "#ffffff",
-			gray: {
-				100: "#101010",
-				200: "#202020",
-				300: "#303030",
-				400: "#404040",
-				500: "#505050"
-			},
-			positive: "#90ee90",
-			negative: "#ff6666"
 		}
 	},
 	plugins: [],
